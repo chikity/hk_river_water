71b740f7bafd3ab1c054424a3188db6f8e825836
Chi Kit Yeung <chikit2370@gmail.com>
2022-11-11 01:04:54 +0800

    calculate wqi, visualization draft

--- data_wrangling.R
+++ data_wrangling.R
@@ -1,56 +1,209 @@
 require(tidyverse)
 require(janitor)
+require(lubridate)
+require(plotly)
 
-# Importing the data
-# First define the path where the data is stored
-path <- "./data/"
-filenames <- list.files(path = path)
+# # Importing the data
+# # First define the path where the data is stored
+# path <- "./data/"
+# filenames <- list.files(path = path)
+# 
+# ## Into a list
+# all_df <- lapply(filenames, function(i) {
+#   i <- paste(path,i,sep="")
+#   read.csv(i, header=FALSE)
+# })
+# filenames <- gsub("-","_",filenames)
+# names(all_df) <- gsub(".csv","",filenames)
+# 
+# ## Joining all the data frames
+# joined_df <- all_df %>% 
+#   reduce(full_join)
+# 
+# ## Reduce function above appends a new placeholder column name and pushes the actual name to first row
+# names(joined_df) <- joined_df[1,]
+# df <- joined_df %>% 
+#   slice(-1)
 
-## Into a list
-all_df <- lapply(filenames, function(i) {
-  i <- paste(path,i,sep="")
-  read.csv(i, header=FALSE)
-})
-filenames <- gsub("-","_",filenames)
-names(all_df) <- gsub(".csv","",filenames)
-
-## Joining all the data frames
-joined_df <- all_df %>% 
-  reduce(full_join)
-
-## Reduce function above appends a new placeholder column name and pushes the actual name to first row
-names(joined_df) <- joined_df[1,]
-df <- joined_df %>% 
-  slice(-1)
+df_raw <- read_csv('river-historical-1986_2020-en.csv')
 
 # Basic cleaning
-clean_df <- df %>% 
+df <- df_raw %>%
   clean_names() %>% 
   subset(select = c(water_control_zone, 
                     river, 
                     station, 
                     dates, 
-                    sample_no, 
-                    x5_day_biochemical_oxygen_demand_mg_l, 
-                    chemical_oxygen_demand_mg_l, 
-                    conductivity_m_s_cm, 
+                    sample_no,
+                    turbidity_ntu,
+                    suspended_solids_mg_l,
                     dissolved_oxygen_mg_l, 
-                    e_coli_counts_100m_l, 
-                    faecal_coliforms_counts_100m_l, 
-                    # flow_m3_s, 
-                    oil_and_grease_mg_l, 
-                    suspended_solids_mg_l, 
-                    total_solids_mg_l, 
-                    turbidity_ntu)
+                    x5_day_biochemical_oxygen_demand_mg_l, 
+                    chemical_oxygen_demand_mg_l,
+                    total_organic_carbon_mg_l,
+                    total_phosphorus_mg_l,
+                    total_kjeldahl_nitrogen_mg_l,
+                    faecal_coliforms_counts_100m_l)
          ) %>% 
-  rename(bod5 = x5_day_biochemical_oxygen_demand_mg_l, 
-         cod = chemical_oxygen_demand_mg_l,
-         conductivity = conductivity_m_s_cm,
-         dissolved_oxygen = dissolved_oxygen_mg_l,
-         e_coli = e_coli_counts_100m_l,
-         faecal_coliform = faecal_coliforms_counts_100m_l,
-         fog = oil_and_grease_mg_l,
-         ss = suspended_solids_mg_l,
-         ts = total_solids_mg_l,
-         turbidity = turbidity_ntu
-         )
+  rename(turbidity = turbidity_ntu,
+          ss = suspended_solids_mg_l,
+          dissolved_oxygen = dissolved_oxygen_mg_l,
+          bod5 = x5_day_biochemical_oxygen_demand_mg_l, 
+          cod = chemical_oxygen_demand_mg_l,
+          t_carbon = total_organic_carbon_mg_l,
+          t_phosphorus = total_phosphorus_mg_l,
+          t_nitrogen = total_kjeldahl_nitrogen_mg_l,
+          faecal_coliform = faecal_coliforms_counts_100m_l) %>% 
+  mutate(ss = as.double(ss),
+         bod5 = as.double(bod5),
+         cod = as.double(cod),
+         t_carbon = as.double(t_carbon),
+         t_carbon = if_else(is.na(t_carbon), 0.9, t_carbon),
+         t_phosphorus = as.double(t_phosphorus),
+         t_phosphorus = if_else(is.na(t_phosphorus), 0.01, t_phosphorus),
+         t_nitrogen = as.double(t_nitrogen),
+         t_nitrogen = if_else(is.na(t_nitrogen), 0.04, t_nitrogen),
+         faecal_coliform = if_else(faecal_coliform == '<1', '0.9', faecal_coliform),
+         faecal_coliform = as.double(faecal_coliform))
+
+# Calculate subindex (si)
+index_df <- df %>% 
+  mutate(turbidity_si = case_when(
+    turbidity <= 1.5 ~ 10,
+    turbidity > 1.5 & turbidity <= 3.0 ~ 20,
+    turbidity > 3.0 & turbidity <= 4.0 ~ 30,
+    turbidity > 4.0 & turbidity <= 4.5 ~ 40,
+    turbidity > 4.5 & turbidity <= 5.2 ~ 50,
+    turbidity > 5.2 & turbidity <= 8.8 ~ 60,
+    turbidity > 8.8 & turbidity <= 12.2 ~ 70,
+    turbidity > 12.2 & turbidity <= 16.5 ~ 80,
+    turbidity > 16.5 & turbidity <= 21 ~ 90,
+    turbidity > 21 ~ 100
+  )) %>% 
+  mutate(ss_si = case_when(
+    ss <= 2 ~ 10,
+    ss > 2 & ss <= 3 ~ 20,
+    ss > 3 & ss <= 4 ~ 30,
+    ss > 4 & ss <= 5.5 ~ 40,
+    ss > 5.5 & ss <= 6.5 ~ 50,
+    ss > 6.5 & ss <= 9.5 ~ 60,
+    ss > 9.5 & ss <= 12.5 ~ 70,
+    ss > 12.5 & ss <= 18 ~ 80,
+    ss > 18 & ss <= 26.5 ~ 90,
+    ss > 26.5 ~ 100
+  )) %>% 
+  mutate(dissolved_oxygen_si = case_when(
+    dissolved_oxygen >= 8 ~ 10,
+    dissolved_oxygen < 8 & dissolved_oxygen >= 7.3 ~ 20,
+    dissolved_oxygen < 7.3 & dissolved_oxygen >= 6.7 ~ 30,
+    dissolved_oxygen < 6.7 & dissolved_oxygen >= 6.3 ~ 40,
+    dissolved_oxygen < 6.3 & dissolved_oxygen >= 5.8 ~ 50,
+    dissolved_oxygen < 5.8 & dissolved_oxygen >= 5.3 ~ 60,
+    dissolved_oxygen < 5.3 & dissolved_oxygen >= 4.8 ~ 70,
+    dissolved_oxygen < 4.8 & dissolved_oxygen >= 4 ~ 80,
+    dissolved_oxygen < 4 & dissolved_oxygen >= 3.1 ~ 90,
+    dissolved_oxygen < 3.1 ~ 100
+  )) %>% 
+  mutate(bod5_si = case_when(
+    bod5 <= 0.8 ~ 10,
+    bod5 > 0.8 & bod5 <= 1 ~ 20,
+    bod5 > 1 & bod5 <= 1.1 ~ 30,
+    bod5 > 1.1 & bod5 <= 1.3 ~ 40,
+    bod5 > 1.3 & bod5 <= 1.5 ~ 50,
+    bod5 > 1.5 & bod5 <= 1.9 ~ 60,
+    bod5 > 1.9 & bod5 <= 2.3 ~ 70,
+    bod5 > 2.3 & bod5 <= 3.3 ~ 80,
+    bod5 > 3.3 & bod5 <= 5.1 ~ 90,
+    bod5 > 5.1 ~ 100
+  )) %>% 
+  mutate(cod_si = case_when(
+    cod <= 16 ~ 10,
+    cod > 16 & cod <= 24 ~ 20,
+    cod > 24 & cod <= 32 ~ 30,
+    cod > 32 & cod <= 38 ~ 40,
+    cod > 38 & cod <= 46 ~ 50,
+    cod > 46 & cod <= 58 ~ 60,
+    cod > 58 & cod <= 72 ~ 70,
+    cod > 72 & cod <= 102 ~ 80,
+    cod > 102 & cod <= 146 ~ 90,
+    cod > 146 ~ 100
+  )) %>% 
+  mutate(t_carbon_si = case_when(
+    t_carbon <= 5 ~ 10,
+    t_carbon > 5 & t_carbon <= 7 ~ 20,
+    t_carbon > 7 & t_carbon <= 9.5 ~ 30,
+    t_carbon > 9.5 & t_carbon <= 12 ~ 40,
+    t_carbon > 12 & t_carbon <= 14 ~ 50,
+    t_carbon > 14 & t_carbon <= 17.5 ~ 60,
+    t_carbon > 17.5 & t_carbon <= 21 ~ 70,
+    t_carbon > 21 & t_carbon <= 27.5 ~ 80,
+    t_carbon > 27.5 & t_carbon <= 37 ~ 90,
+    t_carbon > 37 ~ 100
+  )) %>% 
+  mutate(t_nitrogen_si = case_when(
+    t_nitrogen <= 0.55 ~ 10,
+    t_nitrogen > 0.55 & t_nitrogen <= 0.75 ~ 20,
+    t_nitrogen > 0.75 & t_nitrogen <= 0.9 ~ 30,
+    t_nitrogen > 0.9 & t_nitrogen <= 1 ~ 40,
+    t_nitrogen > 1 & t_nitrogen <= 1.2 ~ 50,
+    t_nitrogen > 1.2 & t_nitrogen <= 1.4 ~ 60,
+    t_nitrogen > 1.4 & t_nitrogen <= 1.6 ~ 70,
+    t_nitrogen > 1.6 & t_nitrogen <= 2 ~ 80,
+    t_nitrogen > 2 & t_nitrogen <= 2.7 ~ 90,
+    t_nitrogen > 2.7 ~ 100
+  )) %>% 
+  mutate(t_phosphorus_si = case_when(
+    t_phosphorus <= 0.02 ~ 10,
+    t_phosphorus > 0.02 & t_phosphorus <= 0.03 ~ 20,
+    t_phosphorus > 0.03 & t_phosphorus <= 0.05 ~ 30,
+    t_phosphorus > 0.05 & t_phosphorus <= 0.07 ~ 40,
+    t_phosphorus > 0.07 & t_phosphorus <= 0.09 ~ 50,
+    t_phosphorus > 0.09 & t_phosphorus <= 0.16 ~ 60,
+    t_phosphorus > 0.16 & t_phosphorus <= 0.24 ~ 70,
+    t_phosphorus > 0.24 & t_phosphorus <= 0.46 ~ 80,
+    t_phosphorus > 0.46 & t_phosphorus <= 0.89 ~ 90,
+    t_phosphorus > 0.89 ~ 100
+  )) %>% 
+  mutate(faecal_coliform_si = case_when(
+    faecal_coliform <= 10 ~ 10,
+    faecal_coliform > 10 & faecal_coliform <= 20 ~ 20,
+    faecal_coliform > 20 & faecal_coliform <= 35 ~ 30,
+    faecal_coliform > 35 & faecal_coliform <= 55 ~ 40,
+    faecal_coliform > 55 & faecal_coliform <= 75 ~ 50,
+    faecal_coliform > 75 & faecal_coliform <= 135 ~ 60,
+    faecal_coliform > 135 & faecal_coliform <= 190 ~ 70,
+    faecal_coliform > 190 & faecal_coliform <= 470 ~ 80,
+    faecal_coliform > 470 & faecal_coliform <= 960 ~ 90,
+    faecal_coliform > 960 ~ 100,
+    is.na(faecal_coliform) ~ 50
+  ))
+  
+index_df$wqi <- round(rowMeans(subset(index_df, select = c(turbidity_si, ss_si, dissolved_oxygen_si, bod5_si, cod_si, t_carbon_si, t_nitrogen_si, t_phosphorus_si, faecal_coliform_si)), na.rm = TRUE))
+
+  
+df2 <- index_df %>% 
+  mutate(year = year(dates)) %>% 
+  mutate(month = month(dates)) %>% 
+  subset(select = c(water_control_zone, river, station, year, month, wqi)) %>% 
+  mutate(quality = case_when(
+    wqi >= 0 & wqi < 45 ~ 'good',
+    wqi >= 45 & wqi < 60 ~ 'fair',
+    wqi >= 60	~ 'poor'
+  ))
+
+river <- df2 %>% 
+  group_by(water_control_zone, river, year) %>% 
+  summarise(wqi = round(mean(wqi))) 
+  # filter(year>= 2010)
+         
+p <- river %>% 
+  filter(water_control_zone == 'Deep Bay') %>% 
+  ggplot(aes(x = year, y = wqi, color = river)) +
+  geom_line() +
+  theme_bw()
+  # facet_wrap(vars(water_control_zone))
+
+ggplotly(p)
+
+
+
